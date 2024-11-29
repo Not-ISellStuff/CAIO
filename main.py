c4b0a77a0696d4d9da777d35a687d34bd9eb95d2
ISellStuff <nataliag123j@gmail.com>
2024-11-28 18:33:16 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+
+|  _______________________________________
+| |                                       |
+| |          CAIO Version 1.0             |   
+| |          Dev -> ISellStuff            |           
+| |                                       |
+| |_______________________________________|
+|
+|
+|
+|   CAIO - Cracking All In One
+|
+|   This tool was made for account cracking
+|   If you like CAIO please leave a star on this repo
+|   
+|   Enjoy ;)
+
+"""
+
+from Modules.caio import CAIO
+
+def main():
+    caio = CAIO()
+    caio.menu()
+
+if __name__ == "__main__":
+    main()
